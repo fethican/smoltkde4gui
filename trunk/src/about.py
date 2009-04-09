2e47263afb66a61a57c4dbc661c29bcd6273e173
fethicanc <fethicanc@a552fab0-2480-11de-9353-0b00b05e739f>
2009-04-09 20:28:04 +0000

    add copyright

--- trunk/src/about.py
+++ trunk/src/about.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+# smoltkde4gui - KDE4 user interface for Smolt
 #
-# This program is free software; you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free
-# Software Foundation; either version 2 of the License, or (at your option)
-# any later version.
+# Copyright (C) 2009,  Fethican Coşkuner
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 #
 # Please read the COPYING file.
-#
 
 # PyKDE4 Stuff
 from PyKDE4.kdecore import *
