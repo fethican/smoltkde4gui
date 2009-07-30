f9c214abcf5b65eb206cb711e17d8ca5eddc59a4
fethicanc <fethicanc@a552fab0-2480-11de-9353-0b00b05e739f>
2009-07-30 22:13:33 +0000

    fix import and change to KUniqueApplication.

--- trunk/src/main.py
+++ trunk/src/main.py
@@ -16,16 +16,14 @@ import sys
 from PyKDE4.kdeui import *
 from PyKDE4.kdecore import *
 from about import aboutData
-from smoltKdeGui import MainWindow
-
+from mainWindow import MainWindow
 
 if __name__ == "__main__":
-	
+
     KCmdLineArgs.init(sys.argv, aboutData)
 
-    app = KApplication()
+    app = KUniqueApplication()
 
     mainWindow = MainWindow()
     mainWindow.show()
-
     app.exec_()
