281ee6c32928cb049b3b49ade5f21d89d7a45b08
fethicanc <fethicanc@a552fab0-2480-11de-9353-0b00b05e739f>
2009-08-27 08:30:19 +0000

    rights

--- trunk/src/infoDialog.py
+++ trunk/src/infoDialog.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# smoltkde4gui - KDE4 user interface for Smolt
+#
+# Copyright (C) 2009,  Fethican Coşkuner
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Please read the COPYING file.
+
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 
